a6a3b868353278f93594ca3f06f2027a847f23ab
yangyr17 <2017011071@secoder.net>
2019-10-08 12:38:55 +0800

    [fix] 正确修改test_backend.py

--- backend/painter/views.py
+++ backend/painter/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from painter.models import User, EmailVerifyRecord
 from django.views.decorators.csrf import csrf_exempt
 from shutil import copyfile
@@ -25,29 +25,29 @@ def register(request):
     avatar: 头像图片，可以不上传，此时将使用默认头像
     """
     if request.method != 'POST':
-        return HttpResponse('Not POST', status = 400)
+        return JsonResponse({'message': 'Not POST'}, status = 400)
     if request.POST.get('username') == None:
-        return HttpResponse('No username', status = 400)
+        return JsonResponse({'message': 'No username'}, status = 400)
     if request.POST.get('password') == None:
-        return HttpResponse('No password', status = 400)
+        return JsonResponse({'message': 'No password'}, status = 400)
     if request.POST.get('nickname') == None:
-        return HttpResponse('No nickname', status = 400)
+        return JsonResponse({'message': 'No nickname'}, status = 400)
     if request.POST.get('email') == None:
-        return HttpResponse('No email', status = 400)
+        return JsonResponse({'message': 'No email'}, status = 400)
     username = request.POST.get('username')
     password = request.POST.get('password')
     nickname = request.POST.get('nickname')
     email = request.POST.get('email')
     if len(username) > NAME_MAX_LEN or username.replace('_', '0').isalnum() == False:
-        return HttpResponse('Username format error', status = 400)
+        return JsonResponse({'message': 'Username format error'}, status = 400)
     if len(password) > NAME_MAX_LEN or password.replace('_', '0').isalnum() == False:
-        return HttpResponse('Password format error', status = 400)
+        return JsonResponse({'message': 'Password format error'}, status = 400)
     if len(nickname) > NAME_MAX_LEN:
-        return HttpResponse('Nickname format error', status = 400)
+        return JsonResponse({'message': 'Nickname format error'}, status = 400)
     if re.match(r'^[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+){0,4}@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+){0,4}$', email) == False:
-        return HttpResponse('Email format error', status = 400)
+        return JsonResponse({'message': 'Email format error'}, status = 400)
     if len(User.objects.filter(username = username)) != 0:
-        return HttpResponse('Repeat username', status = 400)
+        return JsonResponse({'message': 'Repeat username'}, status = 400)
     if request.FILES.get('avatar') == None:
         copyfile(os.path.join(settings.AVATAR_ROOT,'^default.jpg'),os.path.join(settings.AVATAR_ROOT,username+'.jpg'))
         User.objects.create(username = username, password = password, nickname = nickname, email = email)
@@ -59,7 +59,7 @@ def register(request):
     code = random_str(20)
     send_email(email, username, code)
     EmailVerifyRecord.objects.create(username = username, code = code, email = email, send_type = 1)
-    return HttpResponse('Register successfully, please verify your account by email in 72 hours.', status = 200)
+    return JsonResponse({'message': 'Register successfully, please verify your account by email in 72 hours.'}, status = 200)
 
 
 @csrf_exempt
@@ -71,22 +71,22 @@ def login(request):
     TODO
     """
     if request.method != 'POST':
-        return HttpResponse('Not POST', status = 400)
+        return JsonResponse({'message': 'Not POST'}, status = 400)
     if request.POST.get('username') == None:
-        return HttpResponse('No username', status = 400)
+        return JsonResponse({'message': 'No username'}, status = 400)
     if request.POST.get('password') == None:
-        return HttpResponse('No password', status = 400)
+        return JsonResponse({'message': 'No password'}, status = 400)
     username = request.POST.get('username')
     password = request.POST.get('password')
     items = User.objects.filter(username = username)
     if len(items) == 0:
-        return HttpResponse('No such user', status = 400)
+        return JsonResponse({'message': 'No such user'}, status = 400)
     item = items[0]
     if item.password != password:
-        return HttpResponse('Wrong password', status = 400)
+        return JsonResponse({'message': 'Wrong password'}, status = 400)
     if item.valid == False:
-        return HttpResponse('The user has not been validated.', status = 400)
-    return HttpResponse('Login successfully', status = 200)
+        return JsonResponse({'message': 'The user has not been validated.'}, status = 400)
+    return JsonResponse({'message': 'Login successfully'}, status = 200)
 
 
 def validate(request):
@@ -97,21 +97,21 @@ def validate(request):
     TODO
     """
     if request.method != 'GET':
-        return HttpResponse('Not GET', status = 400)
+        return JsonResponse({'message': 'Not GET'}, status = 400)
     if request.GET.get('username') == None:
-        return HttpResponse('No username', status = 400)
+        return JsonResponse({'message': 'No username'}, status = 400)
     if request.GET.get('code') == None:
-        return HttpResponse('No code', status = 400)
+        return JsonResponse({'message': 'No code'}, status = 400)
     username = request.GET.get('username')
     code = request.GET.get('code')
     items = EmailVerifyRecord.objects.filter(username = username, code = code, send_type = 1)
     if len(items) == 0:
-        return HttpResponse('Verify Failed.', status = 400)
+        return JsonResponse({'message': 'Verify Failed.'}, status = 400)
     item = items[0]
     if (datetime.now(timezone.utc) - item.send_time).seconds / 60 / 60 > settings.CONFIRM_HOURS:
-        return HttpResponse('Expired', status = 400)
+        return JsonResponse({'message': 'Expired'}, status = 400)
     user = User.objects.filter(username = username)[0]
     user.valid = True
     user.save()
     item.delete()
-    return HttpResponse('Verify Successfully.', status = 200)
+    return JsonResponse({'message': 'Verify Successfully.'}, status = 200)
